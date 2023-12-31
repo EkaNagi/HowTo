020bfb674141883d74855c80b0c7a513397e9ac3
Tresna Eka Widiana <ideal.nagi@gmail.com>
2023-12-31 09:29:20 +0700

    view kategori

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,17 +2,19 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">Post Categories</h1>
+    <h1 class="text-3xl font-bold text-gray-900 sm:text-4xl text-center">Post Categories</h1>
+    <p class="mt-2 text-lg leading-8 text-gray-600 text-center">I wonder, what exactly are you looking for ?</p>
+    <hr class="mb-5 mt-2">
 
-    <div class="container">
+    <div class="container columns-3">
         <div class="row">
             @foreach($categories as $category)
-            <div class="col-md-4">
+            <div class="px-20">
                 <a href="/posts?category={{ $category->slug }}">
-                <div class="card text-bg-dark">
-                    <img src="https://source.unsplash.com/500x500?{{ $category->name }}" class="card-img" alt="{{ $category->name }}    ">
-                    <div class="card-img-overlay d-flex align-items-center p-0">
-                      <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0,0,0,0.7)">{{ $category->name }}</h5>
+                <div class="card">
+                    <img src="https://source.unsplash.com/500x500?{{ $category->name }}" class="rounded" alt="{{ $category->name }}    ">
+                    <div class="items-center">
+                      <h5 class="mt-5 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xlt text-center">{{ $category->name }}</h5>
                     </div>
                 </div>
                 </a>
