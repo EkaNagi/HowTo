1bc89c6c7e6fe77191e1abca2888a342efd847b7
Tresna Eka Widiana <ideal.nagi@gmail.com>
2023-12-31 11:09:25 +0700

    form login view

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,7 +2,52 @@
 
 @section('container')
 
-<div class="row justify-content-center">
+<div class="flex min-h-full flex-col justify-center px-6 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <h2 class="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Ayo login!</h2>
+  </div>
+
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form class="space-y-6" action="/login" method="post">
+      @csrf
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div class="mt-2">
+          <input id="email" name="email" type="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('email') is-invalid @enderror" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+        </div>
+          @error('email')
+            <div class="">
+              {{ $message }}
+            </div>
+          @enderror
+        </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+        </div>
+        <div class="mt-2">
+          <input id="password" name="password" type="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password" required>
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+      </div>
+    </form>
+
+    <p class="mt-10 text-center text-sm text-gray-500">
+      Belum join kita?
+      <a href="/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Daftar cepet!</a>
+      <br>
+      Atau, 
+      <a href="{{ route('auth.google') }}" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">login dengan google?</a>
+    </p>
+  </div>
+</div>
+
+
+{{-- <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <div class="col-md-4">
       @if(session()->has('success'))
       <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -18,31 +63,32 @@
       @endif
         <main class="form-signin w-100 m-auto">
             <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
-            <form action="/login" method="post">
+            <form class="space-y-6" action="/login" method="post">
               @csrf
-              <div class="form-floating">
-                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
-                <label for="email">Email address</label>
+              <div class="">
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                <input type="email" name="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
                 @error('email')
                 <div class="invalid-feedback">
                   {{ $message }}
                 </div>
                 @enderror
               </div>
-              <div class="form-floating">
-                <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
-                <label for="password">Password</label>
+              <div class="">
+                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                <input type="password" name="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="password" placeholder="Password" required>
               </div>
-              <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
+              <button class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit">Login</button>
             </form>
-            <small class="d-block text-center mt-3">
-                Not registered? <a href="/register">Register Now!</a>
-                <hr>
-                <a href="{{ route('auth.google') }}">Login dengan google</a>
-            </small>
+            <p class="mt-10 text-center text-sm text-gray-500 mb-3">
+                Not registered? <a href="/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Register Now!</a>
+                <br>
+                Atau,
+                <a href="{{ route('auth.google') }}" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login dengan google</a>
+            </p>
           </main>
     </div>
-</div>
+</div> --}}
     
 
 
