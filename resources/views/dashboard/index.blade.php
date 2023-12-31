c7ebe03cc4f141d7bfb1ce5fafa655f7ac12f51e
Tresna Eka Widiana <ideal.nagi@gmail.com>
2023-12-31 19:59:40 +0700

    delete semua file di repository
    kesalahan dalam deploy

--- resources/views/dashboard/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('dashboard.layouts.main')
-
-@section('container')
-<div class="text-3xl font-bold text-gray-900 sm:text-4xl text-center pt-60">
-    <h1 class="">Welcome back, {{ auth()->user()->name }}!</h1>
-    <p class="mt-2 text-lg leading-8 text-gray-600">To make a guide, go to my guide menu.</p>
-</div>
-@endsection
